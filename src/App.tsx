a4ef4e28cac2aa5ed53d2a6fd31d73b3f698406c
jhuhnke <jessica@flipsidecrypto.com>
2023-04-19 17:16:09 -0500

    Build up index

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import { Route, useLocation } from 'react-router'
+import { Routes } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { useState } from 'react'
+import './App.css'
+
